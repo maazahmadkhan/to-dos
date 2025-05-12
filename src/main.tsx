74fc99f13f70339119381526332d2dd57573447b
Maaz Ahmad Khan <maazahmadkhan@superfone.in>
2025-05-12 14:38:46 +0530

    bug fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { registerSW } from "virtual:pwa-register";
-
-registerSW({
-  onNeedRefresh() {},
-  onOfflineReady() {
-    console.log("App is ready to work offline");
-  },
-});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
