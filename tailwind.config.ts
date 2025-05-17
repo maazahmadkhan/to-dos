da65cd678271b7a9497f93f30a43536f33f7f10b
Maaz Ahmad Khan <maazahmadkhan@superfone.in>
2025-05-18 00:17:45 +0530

    added postcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-	content: ['./src/**/*.{html,js,ts,svelte}', './src/routes/**/*.{svelte,js,ts}'],
+	content: [
+		'./src/**/*.{html,svelte,js,ts}', // Adjust based on your custom folder setup
+		'./src/routes/**/*.{svelte,js,ts}' // Ensure routes are included if custom folder
+	],
 	theme: {
 		extend: {}
 	},
--- vite.config.ts
+++ vite.config.ts
@@ -29,5 +29,8 @@ export default defineConfig({
 				]
 			}
 		})
-	]
+	],
+	css: {
+		postcss: './postcss.config.cjs' // or correct path
+	}
 });
